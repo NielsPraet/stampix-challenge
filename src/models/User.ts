c86316d069d86841690b35f3f3c341bf8b0d565a
Niels Praet <niels.praet@ugent.be>
2021-05-08 13:59:03 +0200

    refactor: put user interface in seperate class

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+export default interface User {
+  gender: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  date_of_birth: string;
+  language: string;
+  created_at: Date;
+  modified_at: Date;
+}
