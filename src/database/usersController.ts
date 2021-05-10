79ffa11af4ca7c39e1c1e9322f9e7c0786ccf15d
Niels Praet <niels.praet@ugent.be>
2021-05-10 16:51:51 +0200

    feat: made it possible to fetch user by id

--- src/database/usersController.ts
+++ src/database/usersController.ts
@@ -13,11 +13,13 @@ export default class UsersController {
   ) => {
     try {
       let users;
+      // if a filter exists, we fetch all users where first name is equal to filter
       if (filter) {
         users = await knexInstance<User>('user')
           .select('*')
           .where('first_name', filter);
       } else {
+        // otherwise we just fetch all users
         users = await knexInstance<User>('user').select('*');
       }
       return users;
@@ -29,12 +31,30 @@ export default class UsersController {
   /**
    * function that adds the given user to the database
    * @param user The new user we want to add to the database
-   * @returns
+   * @returns the id of the new user
    */
-  static createUser: (user: User) => Promise<void> = async (user: User) => {
+  static createUser: (user: User) => Promise<number> = async (user: User) => {
     try {
       // inserting values will return a list of the indexes of all users added to the database
       return await knexInstance<User>('user').insert(user);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  /**
+   * function that fetches the user with the given id from the database and returns it
+   * @param id The unique identifier for a certain user -> currently just the row index
+   * @returns the user identified by the id
+   */
+  static getUser: (id: number) => Promise<User> = async (id: number) => {
+    try {
+      // returns an array of all users where id field is equal to given id
+      const userList = await knexInstance<User>('user')
+        .select('*')
+        .where('ROWID', id);
+      // because the id should be unique, we can safely return the first element of this array
+      return userList[0];
     } catch (e) {
       return Promise.reject(e);
     }
--- src/get.ts
+++ src/get.ts
@@ -1,5 +1,54 @@
+import UsersController from './database/usersController';
+import Utils from './Utils';
 // Your project code
 
-exports.handler = () => {
-  // Your code here
+exports.handler = (
+  event: any,
+  _context: any,
+  callback: (p1: any, p2?: any) => any
+) => {
+  const path_parameters = event.pathParameters;
+  const id = path_parameters ? path_parameters.id : null;
+  // if no id is given, we return an error
+  if (!id) {
+    callback('No ID was specified');
+  }
+  users_get(id)
+    .then((data) => callback(null, data))
+    .catch((err) => callback(err));
+};
+
+/**
+ * main part of the code for this endpoint is placed here
+ * @param id the unique identifier for the requested user
+ * @returns a valid response object with the user in its body
+ */
+const users_get: (id: number) => Promise<any> = async (id: number) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      // get user with id
+      const user = await UsersController.getUser(id);
+      // create the response
+      let response;
+      if (user) {
+        response = Utils.createResponse(200, headers, JSON.stringify(user));
+      } else {
+        response = Utils.createResponse(
+          404,
+          headers,
+          JSON.stringify({ error: 'not found' })
+        );
+      }
+      resolve(response);
+    } catch (e) {
+      // create the error response
+      // for now, we are going to assume we get an error because no user with that id could be found
+      const response = Utils.createResponse(500, headers, JSON.stringify(e));
+      reject(response);
+    }
+  });
 };
